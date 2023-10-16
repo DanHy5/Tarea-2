9e701ed3848543b3ba92566daf8d214ca65ba8b8
DanHy5 <tapia7203@gmail.com>
2023-10-16 18:37:45 -0500

    first commit

---
+++ tareas.js
@@ -0,0 +1,26 @@
+let nombre = "Danny";
+let edad = 24;
+console.log("tu Nombre es: " + nombre);
+console.log("tu Nombre es: " + edad);
+
+let PrecioProducto = 155;
+console.log("tu Nombre es: " + edad);
+
+let numb1 = 123;
+let numb2 = 345;
+
+let c = numb1 + numb2;
+
+console.log("tu Nombre es: " + c);
+
+let celcius = 25;
+
+let fa = celcius * 9/5 + 32;
+
+console.log("tu Nombre es: " + fa);
+
+
+
+
+
+
